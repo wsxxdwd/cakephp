40d83b54000937673a67a2ea2f2358ee181670cb
wsxxdwd <wsxxdwdd@gmail.com>
2013-10-28 17:17:51 +0800

    init core.php

--- app/Config/core.php
+++ app/Config/core.php
@@ -224,12 +224,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'JHDJsbjfBFJFJDSAJH8721U31DSA882BAMssdfg2');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '37481897593717386168384183848317733');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
